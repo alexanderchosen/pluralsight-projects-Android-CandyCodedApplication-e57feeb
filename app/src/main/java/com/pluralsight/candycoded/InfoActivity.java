b9c3a11bb6c490e562dd10a935466def05629950
Alexander Chosen okon <chosenokonalexander@gmail.com>
2020-10-12 17:07:57 +0000

    Create mapIntent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -31,8 +31,8 @@ public class InfoActivity extends AppCompatActivity {
 
     public void createMapIntent(View view) {
 
-        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+        Uri mapUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
         mapIntent.setPackage("com.google.android.apps.maps");
         if (mapIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(mapIntent);
